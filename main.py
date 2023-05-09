3d15d71bcbd4fb67159781767dea6b4c0632ecd0
Parth Gupta <parthgupta3003@gmail.com>
2023-05-09 23:56:39 +0530

    worked and generated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pdf2final_list
 import text2ppt
 
-x=pdf2final_list.process(["Logistic Regression"])
+x=pdf2final_list.process(["Logistic Regression","Estimating Probabilities in ML", "Training and cost function in ML", "Decision boundaries in ML", "SoftMax regression", "Non-linear SVM classification","Polynomial kernel in SVM", "adding similarity feature in SVM", "Gaussian RBF Kernel in SVM", "SVM Regression"])
 text2ppt.presentate(x)
